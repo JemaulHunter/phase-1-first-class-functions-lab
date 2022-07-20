f4052923d5a0a5a1af8a2b2d6929fd1c7113401d
Jemaul Hunter <jemaulhunter@gmail.com>
2022-07-20 06:42:35 -0400

    commit tree f8b919598ebe417ec9a0ec21a748949bbc78772f

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = drivers.slice(0, 2);
+const returnLastTwoDrivers = drivers.slice(-2)
+displayDrivers.apply(returnFirstTwoDrivers);
+    function displayDrivers(){
+        
+    }
+displayDrivers.apply(returnLastTwoDrivers);
+    function displayDrivers(){
+        
+    }
+const selectingDrivers = returnFirstTwoDrivers.concat(returnLastTwoDrivers);
